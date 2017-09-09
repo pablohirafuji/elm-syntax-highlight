da9fde00fff8eb95778bb161dd850e73e32b0a1a
Pablo Hirafuji <pablohirafuji@gmail.com>
2017-09-08 23:04:15 -0300

    Add new themes list page

--- demo/Themes.elm
+++ demo/Themes.elm
@@ -1,102 +1,47 @@
-module Themes exposing (view)
+port module Themes exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (attribute, id, href, class)
 import SyntaxHighlight.Theme as Theme
 
 
-main : Html msg
+main : Program Never () Msg
 main =
-    view
+    Platform.program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
 
 
-view : Html msg
-view =
-    node "html"
-        []
-        [ headerView
-        , bodyView
-        ]
+
+-- TYPES
 
 
-headerView : Html msg
-headerView =
-    node "head"
-        []
-        [ node "meta" [ attribute "charset" "utf-8" ] []
-        , node "meta"
-            [ attribute "name" "viewport"
-            , attribute "content" "width=device-width, initial-scale=1"
-            ]
-            []
-        , node "title" [] [ text "Elm Syntax Highlight - Themes" ]
-        , node "style" [] [ text styleView ]
-        ]
+type alias Msg =
+    ()
 
 
-styleView : String
-styleView =
-    """body {
-    margin: 40px auto;
-    max-width: 650px;
-    line-height: 1.6;
-    font-size: 18px;
-    color: #444;
-    padding: 0 10px;
-}
-h1,h2,h3 {
-    line-height: 1.2
-}
-h1 {
-    text-align: center;
-    padding-bottom: 0;
-    margin-bottom: 0;
-}
-.subheading {
-    margin-top: 0;
-    text-align: center;
-}"""
+type alias ThemeModel =
+    { name : String
+    , content : String
+    }
 
 
-bodyView : Html msg
-bodyView =
-    body []
-        [ h1 [] [ text "Elm Syntax Highlight Themes" ]
-        , p [ class "subheading" ]
-            [ a [ href "http://package.elm-lang.org/packages/pablohirafuji/elm-syntax-highlight/latest" ] [ text "Package" ]
-            , text " / "
-            , a [ href "https://github.com/pablohirafuji/elm-syntax-highlight" ] [ text "GitHub" ]
-            , text " / "
-            , a [ href "https://pablohirafuji.github.io/elm-syntax-highlight/" ] [ text "Demo" ]
-            ]
-        , tableOfContent
-        , h2 [ id "Required-Styles" ] [ text "Required Styles" ]
-        , requiredStylesView
-        , themesView
-        ]
+init : ( (), Cmd Msg )
+init =
+    ( ()
+    , themesList
+        (List.map
+            (\( name, content ) ->
+                ThemeModel name (String.trim content)
+            )
+            (requiredStyles :: Theme.all)
+        )
+    )
 
 
-tableOfContent : Html msg
-tableOfContent =
-    List.map (Tuple.first >> tableOfContentHelper) Theme.all
-        |> (::) (li [] [ a [ href "#Required-Styles" ] [ text "Required Styles" ] ])
-        |> ul []
-
-
-tableOfContentHelper : String -> Html msg
-tableOfContentHelper name =
-    li [] [ a [ href ("#" ++ nameToId name) ] [ text name ] ]
-
-
-requiredStylesView : Html msg
-requiredStylesView =
-    pre [] [ code [] [ text requiredStyles ] ]
-
-
-requiredStyles : String
+requiredStyles : ( String, String )
 requiredStyles =
-    """
-pre.elmsh {
+    ( "Required Styles", """pre.elmsh {
     padding: 10px;
     margin: 0;
     text-align: left;
@@ -112,29 +57,12 @@ code.elmsh {
     width: 40px;
     padding: 0 20px 0 0;
     opacity: 0.3;
-}"""
+}""" )
 
 
-themesView : Html msg
-themesView =
-    div [] <|
-        List.map themeView Theme.all
+update : Msg -> () -> ( (), Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
 
 
-themeView : ( String, String ) -> Html msg
-themeView ( name, css ) =
-    div []
-        [ h2 [ id (nameToId name) ] [ text name ]
-        , pre [] [ code [] [ text css ] ]
-        ]
-
-
-nameToId : String -> String
-nameToId =
-    String.map
-        (\c ->
-            if c == ' ' then
-                '-'
-            else
-                c
-        )
+port themesList : List ThemeModel -> Cmd msg
---
+++ demo/make-themes.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+var path = require("path");
+var templateFile = fs.readFileSync(path.resolve(__dirname + "/themes-template.html"));
+var templateStr = templateFile.toString();
+
+
+var elm = require("./elm-themes").Themes.worker();
+elm.ports.themesList.subscribe(function(themeList){
+	var themeListHtml = ""
+	themeList.forEach(function(element, index, array) {
+    	themeListHtml += "<li><details><summary>" + element.name + "</summary><pre class=\"elmsh\"><code>"+ element.content +"</code></pre></details></li>"
+	});
+
+	var themesHtml = templateStr.replace("${themeList}", themeListHtml)
+
+	fs.writeFile("./themes.html", themesHtml, function(err) {
+		if(err) { return console.log(err); }
+	});
+});
