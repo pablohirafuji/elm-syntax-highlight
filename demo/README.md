f5f02f79edc8743e75a9c67e9580d5f2a5d15382
Pablo Hirafuji <pablohirafuji@gmail.com>
2025-03-05 12:38:47 -0300

    Move CI to nix

--- demo/README.md
+++ demo/README.md
@@ -11,15 +11,6 @@ elm reactor
 Open your browser at http://localhost:8000/ and navigate to `src/Main.elm`.
 
 
-## Build
-
-Build the project:
-
-```sh
-node build
-```
-
-
 ## Run standalone
 
 Remove `"../src"` at the `source-directories` entry in `elm.json` file and install the package running:
--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
 
   outputs = { self, nixpkgs }:
     let
@@ -19,9 +19,32 @@
               elm-test
               elm-format
             ]) ++ (with pkgs; [
-              rnix-lsp
+              nil
+              nixpkgs-fmt
               nodejs_20
               nodePackages.uglify-js
+
+              (writeScriptBin "run-demo" ''
+                cd demo
+                ${lib.getExe elmPackages.elm} reactor
+              '')
+
+              (writeScriptBin "build-demo" ''
+                cd demo
+                ${lib.getExe elmPackages.elm} make src/Main.elm --optimize --output build/index.js
+                cp -f index.html build/index.html
+                ${lib.getExe nodePackages.uglify-js} build/index.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | ${lib.getExe nodePackages.uglify-js} --mangle --output build/index.js
+              '')
+
+              (writeScriptBin "build-themes" ''
+                cd demo/themes-page
+                elm make Main.elm --output elm-themes.js
+                node make-themes.js
+                rm elm-themes.js
+                cd ..
+                mv -f themes-page/themes.html build
+              '')
+
             ]);
         };
       });
