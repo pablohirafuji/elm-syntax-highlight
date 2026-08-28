4af85eadfaf5184496dc9d6af7e6de9f4e0b747f
agj <ale@agj.cl>
2026-08-28 11:34:16 -0400

    Change custom API to take `Parser (List Fragment)`

--- src/SyntaxHighlight/Custom.elm
+++ src/SyntaxHighlight/Custom.elm
@@ -1,70 +1,24 @@
-module SyntaxHighlight.Custom exposing (Fragment, Line, Style, fragment, fromParser, line, setFragmentClasses, setFragmentStyle, setLineHighlight, style1, style2, style3, style4, style5, style6, style7, styleComment, styleDefault)
+module SyntaxHighlight.Custom exposing (Fragment, Style, fragment, fromParser, lineBreak, setFragmentClasses, setFragmentStyle, style1, style2, style3, style4, style5, style6, style7, styleComment, styleDefault)
 
 {-| -}
 
 import Parser exposing (Parser)
 import SyntaxHighlight exposing (Highlight(..))
 import SyntaxHighlight.Language.Type exposing (HCode(..))
-import SyntaxHighlight.Line as Line
+import SyntaxHighlight.Line as Line exposing (Line)
 import SyntaxHighlight.Style as Style
 
 
 {-| Use a parser from `elm/parser` to define your own syntax. Your parser must
 produce a list of `Line` values out of the code string.
 -}
-fromParser : Parser (List Line) -> String -> Result (List Parser.DeadEnd) HCode
+fromParser : Parser (List Fragment) -> String -> Result (List Parser.DeadEnd) HCode
 fromParser parser code =
     Parser.run parser code
         |> Result.map
-            (\lines ->
-                HCode (lines |> List.map (\(Line line_) -> line_))
+            (\fragments ->
+                HCode (fragmentsIntoLines emptyLine [] fragments)
             )
-
-
-
--- LINE
-
-
-{-| A line of parsed code. Holds information about its `Fragment`s and if is
-highlighted in any way.
--}
-type Line
-    = Line Line.Line
-
-
-{-| Constructs one line of a `customSyntax` parser, out of a list of `Fragment`
-values, which you can in turn construct using the `fragment` function.
--}
-line : List Fragment -> Line
-line fragments =
-    Line
-        { fragments = fragments |> List.map (\(Fragment fragment_) -> fragment_)
-        , highlight = Nothing
-        }
-
-
-{-| You can make a `Line` in a custom syntax look highlighted, or as a diff
-addition/deletion, using this function.
--}
-setLineHighlight : Maybe Highlight -> Line -> Line
-setLineHighlight highlight_ (Line line_) =
-    let
-        convertedHighlight =
-            highlight_
-                |> Maybe.map
-                    (\hl ->
-                        case hl of
-                            Highlight ->
-                                Line.Normal
-
-                            Add ->
-                                Line.Add
-
-                            Del ->
-                                Line.Del
-                    )
-    in
-    Line { line_ | highlight = convertedHighlight }
 
 
 
@@ -76,6 +30,7 @@ the text being styled, the style and additional class to be applied.
 -}
 type Fragment
     = Fragment Line.Fragment
+    | LineBreak (Maybe Highlight)
 
 
 {-| Constructs a `Fragment` value out of a `String`, which is one part of a
@@ -90,12 +45,22 @@ fragment text =
         }
 
 
+lineBreak : Maybe Highlight -> Fragment
+lineBreak highlightM =
+    LineBreak highlightM
+
+
 {-| Sets a specific style to a `Fragment`, which gives it a different color
 depending on the theme used.
 -}
 setFragmentStyle : Style -> Fragment -> Fragment
-setFragmentStyle (Style style) (Fragment fragment_) =
-    Fragment { fragment_ | requiredStyle = style }
+setFragmentStyle (Style style) fragment_ =
+    case fragment_ of
+        Fragment f ->
+            Fragment { f | requiredStyle = style }
+
+        LineBreak _ ->
+            fragment_
 
 
 {-| You can optionally use this function to give a `Fragment` one or more custom
@@ -103,8 +68,13 @@ CSS classes (separated by spaces), if you want more control over how you style
 your custom syntax.
 -}
 setFragmentClasses : String -> Fragment -> Fragment
-setFragmentClasses classes (Fragment fragment_) =
-    Fragment { fragment_ | additionalClass = classes }
+setFragmentClasses classes fragment_ =
+    case fragment_ of
+        Fragment f ->
+            Fragment { f | additionalClass = classes }
+
+        LineBreak _ ->
+            fragment_
 
 
 
@@ -158,3 +128,83 @@ style6 =
 style7 : Style
 style7 =
     Style Style.Style7
+
+
+
+-- INTERNAL
+
+
+fragmentsIntoLines : Line -> List Line -> List Fragment -> List Line
+fragmentsIntoLines accLine accLines fragments =
+    case fragments of
+        [] ->
+            (reverseLineFragments accLine :: accLines)
+                |> List.reverse
+
+        (LineBreak hl) :: restFragments ->
+            fragmentsIntoLines
+                { emptyLine | highlight = hl |> Maybe.map toInternalHighlight }
+                (reverseLineFragments accLine :: accLines)
+                restFragments
+
+        (Fragment f) :: restFragments ->
+            let
+                ( newAccLine, newLines ) =
+                    splitLFragmentOnLineBreaks f
+                        |> placeLFragmentsOnLines accLine []
+            in
+            fragmentsIntoLines newAccLine (newLines ++ accLines) restFragments
+
+
+splitLFragmentOnLineBreaks : Line.Fragment -> List Line.Fragment
+splitLFragmentOnLineBreaks f =
+    String.split "\n" f.text
+        |> List.map (\text -> { f | text = text })
+
+
+placeLFragmentsOnLines : Line -> List Line -> List Line.Fragment -> ( Line, List Line )
+placeLFragmentsOnLines accLine accLines lFragments =
+    let
+        addLastFragment f =
+            { accLine | fragments = f :: accLine.fragments |> List.reverse }
+    in
+    case lFragments of
+        [] ->
+            ( accLine, accLines )
+
+        [ f ] ->
+            -- Last or single fragment = no line break.
+            placeLFragmentsOnLines
+                { accLine | fragments = f :: accLine.fragments }
+                accLines
+                []
+
+        f :: fNext :: restLFragments ->
+            -- Two fragments = line break.
+            placeLFragmentsOnLines
+                emptyLine
+                (addLastFragment f :: accLines)
+                (fNext :: restLFragments)
+
+
+emptyLine : Line
+emptyLine =
+    { fragments = [], highlight = Nothing }
+
+
+reverseLineFragments : Line -> Line
+reverseLineFragments line =
+    { line | fragments = List.reverse line.fragments }
+
+
+toInternalHighlight : Highlight -> Line.Highlight
+toInternalHighlight hl =
+    case hl of
+        Highlight ->
+            Line.Normal
+
+        Add ->
+            Line.Add
+
+        Del ->
+            Line.Del
